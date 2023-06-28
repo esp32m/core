4379a87fad7d2d304cd675acd30afc0514a07ce3
dyarkovoy <dyarkovoy@gmail.com>
2023-06-28 13:09:13 +0300

    added success box to the captive portal screen

--- esp32m/web-ui/esp32m/ui/src/net/wifi/CaptivePortal.tsx
+++ esp32m/web-ui/esp32m/ui/src/net/wifi/CaptivePortal.tsx
@@ -1,13 +1,42 @@
 import { Navigate } from 'react-router-dom';
-import { Grid } from '@mui/material';
+import { Grid, Typography } from '@mui/material';
 
 import { ScannerBox } from './ScannerBox';
 import { StaInfoBox } from './StaInfoBox';
 import { ApInfoBox } from './ApInfoBox';
 import { TContentPlugin, TRoutesPlugin } from '@ts-libs/ui-base';
 
+import { CardBox } from '@ts-libs/ui-app';
+import { useModuleState } from '../../backend';
+import { Name, WifiMode, TWifiState } from './types';
+
+const SuccessBox = () => {
+  const state = useModuleState<TWifiState>(Name);
+  const { sta, mode = 0 } = state || {};
+  if (!sta || ![WifiMode.Sta, WifiMode.ApSta].includes(mode)) return null;
+  const { ssid, ip = [], bssid } = sta;
+  const [addr] = ip;
+  if (!ssid || !addr) return null;
+  return (
+    <CardBox>
+      <Typography align="center" variant="h6">
+        Connection succeeded!
+      </Typography>
+      <Typography align="center" variant="subtitle1">
+        {`The device is connected to the WiFi access point ${
+          ssid + (bssid ? ` (${bssid})` : '')
+        }`}
+      </Typography>
+      <Typography align="center" variant="subtitle2">
+        {`You can now switch to your local WiFi and access this device via http://${addr}/`}
+      </Typography>
+    </CardBox>
+  );
+};
+
 const component = () => (
   <Grid container>
+    <SuccessBox />
     <ScannerBox />
     <StaInfoBox />
     <ApInfoBox />
