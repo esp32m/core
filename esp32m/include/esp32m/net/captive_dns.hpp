8e336daaf58f028430cf05361602bc199be11fd6
dyarkovoy <dyarkovoy@gmail.com>
2023-07-27 14:23:39 +0300

    move CaptiveDns to esp32m::net namespace

--- esp32m/include/esp32m/net/captive_dns.hpp
+++ esp32m/include/esp32m/net/captive_dns.hpp
@@ -6,21 +6,21 @@
 #include "esp32m/logging.hpp"
 
 namespace esp32m {
+  namespace net {
+    class CaptiveDns : public log::SimpleLoggable {
+     public:
+      CaptiveDns(esp_ip4_addr_t ip);
+      ~CaptiveDns();
+      void enable(bool v);
 
-  class CaptiveDns : public log::SimpleLoggable {
-   public:
-    CaptiveDns(esp_ip4_addr_t ip);
-    ~CaptiveDns();
-    void enable(bool v);
-
-   private:
-    esp_ip4_addr_t _ip;
-    bool _enabled = false;
-    int _sockFd = 0;
-    TaskHandle_t _task;
-    void run();
-    void recv(struct sockaddr_in *premote_addr, char *pusrdata,
-              unsigned short length);
-  };
-
+     private:
+      esp_ip4_addr_t _ip;
+      bool _enabled = false;
+      int _sockFd = 0;
+      TaskHandle_t _task;
+      void run();
+      void recv(struct sockaddr_in *premote_addr, char *pusrdata,
+                unsigned short length);
+    };
+  }  // namespace net
 }  // namespace esp32m
